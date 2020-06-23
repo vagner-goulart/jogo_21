c7fbaa29ee799d4c4508bcb5f3840517d99cf654
vagnerPG <vagnergoulart28@gmail.com>
2020-06-23 17:11:04 -0300

    Update principal_vinte_um.py
    (esse comit vai ficar um tanto curto/diferente porque eu desenvolvi em uma copia do arquivo, e acabei esquecendo de ir juntando ao arquivo principal enquanto  progedia)
    
    Agora o jogo está completo e funcionando normalmente. 0 bugs conhecidos.
    Mecanicas de dividir par e dobrar aposta foram adicionadas.
    Alguns prints foram adicionados e/ou alterados para melhor compreensão.
    Logica_somar_cartas asgora usa apenas um parametro já que soma_final pode ser uma variavel interna retornada no final.

--- principal_vinte_um.py
+++ principal_vinte_um.py
@@ -3,10 +3,12 @@ import math
 from time import sleep
 
 cartas_usuario = []
+cartas_user_dividir = []
 cartas_dealer = []
 
-soma_final_usuario = 0
 soma_final_dealer = 0
+soma_final_usuario = 0
+soma_final_user_dividir = 0
 
 um_baralho = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"] * 4
 baralho_p_jogo = um_baralho * 6
@@ -17,10 +19,13 @@ resultado_final = ""
 
 quant_fichas_usuario = 50
 fichas_apostadas = 0
+fichas_apostadas_dividir = 0
 fichas_obtidas = 0
+dividiu_par = False
+run_uma_vez = True
 
 
-# TODO: colocar isso no final das funcoes
+# TODO: rever se vou usar usar isso
 # def resetar_variaveis():
 #     global cartas_usuario, cartas_dealer, resultado_final, fichas_obtidas, soma_final_dealer
 #
@@ -34,7 +39,7 @@ fichas_obtidas = 0
 
 
 def printar_cartas(cartas_p_printar):
-    if cartas_p_printar == cartas_usuario:
+    if cartas_p_printar == cartas_usuario or cartas_p_printar == cartas_user_dividir:
         print("Suas cartas: ", end="")
     elif cartas_p_printar == cartas_dealer:
         print("Cartas dealer: ", end="")
@@ -48,8 +53,12 @@ def printar_cartas(cartas_p_printar):
 
     if cartas_p_printar == cartas_usuario:
         print(f"({soma_final_usuario})")
+
     elif cartas_p_printar == cartas_dealer:
         print(f"({soma_final_dealer})")
+
+    elif cartas_p_printar == cartas_user_dividir:
+        print(f"({soma_final_user_dividir})")
 
 
 def apostar_fichas():
@@ -66,6 +75,9 @@ def apostar_fichas():
             elif int(input_aposta_fichas) <= 0:
                 print("\nDigite um numero vahlido!\n")
 
+            elif int(input_aposta_fichas) == 1:
+                print("\nA aposta minima é 2 fichas\n")
+
             else:
                 return int(input_aposta_fichas)
 
@@ -73,7 +85,10 @@ def apostar_fichas():
             print("\nDigite apenas numeros!\n")
 
 
-def logica_somar_cartas(cartas_p_somar, soma_final):
+def logica_somar_cartas(cartas_p_somar):
+
+    soma_final = 0
+
     for carta in cartas_p_somar:
 
         if carta == "J" or carta == "Q" or carta == "K":
@@ -96,30 +111,29 @@ def logica_somar_cartas(cartas_p_somar, soma_final):
     return soma_final
 
 
-def definir_mensagem_resultado():
-    if soma_final_usuario < 21 and soma_final_dealer < 21:
-        print(f"\nResultado User: {soma_final_usuario}\n")
+def definir_mensagem_resultado(soma_final):
+    if soma_final < 21 and soma_final_dealer < 21:
+        print(f"\nResultado User: {soma_final}")
         print(f"Resultado Dealer: {soma_final_dealer}\n")
 
-    elif soma_final_usuario > 21 or soma_final_dealer > 21:
-        print("Passou 21")
+    elif soma_final > 21 or soma_final_dealer > 21:
+        dealer_ou_user = "Voce" if soma_final > 21 else "Dealer"
+        print(dealer_ou_user, "passou 21")
 
-    if soma_final_dealer > 21:
-        print("teste1")
-        return "User ganhou"
-
-    elif soma_final_usuario > 21:
+    if soma_final > 21:
         return "Dealer ganhou"
 
+    elif soma_final_dealer > 21:
+        return "User ganhou"
+
     else:
-        if soma_final_dealer > soma_final_usuario or soma_final_usuario > 21:
+        if soma_final_dealer > soma_final or soma_final > 21:
             return "Dealer ganhou"
 
-        elif soma_final_dealer < soma_final_usuario or soma_final_dealer > 21:
-            print("teste2")
+        elif soma_final_dealer < soma_final or soma_final_dealer > 21:
             return "User ganhou"
 
-        elif soma_final_dealer == soma_final_usuario:
+        elif soma_final_dealer == soma_final:
             return "Empate"
 
 
@@ -132,7 +146,7 @@ def calcular_fichas_obtidas(fichas_apostadas_parametro):
     elif "User" in resultado_final:
         guardar_fichas += fichas_apostadas_parametro
 
-        if len(cartas_usuario) == 2 and soma_final_usuario == 21:
+        if len(cartas_usuario) == 2 and soma_final_usuario == 21 and not dividiu_par:
             print("\nBLACKJACK!\n")
             guardar_fichas += math.floor(fichas_apostadas_parametro * 1.5)
 
@@ -142,10 +156,70 @@ def calcular_fichas_obtidas(fichas_apostadas_parametro):
         return 0
 
 
+# TODO: rever a posicao dessa funcao depois
+def dividir_par():
+    print("\nJogo 2:\n")
+
+    global soma_final_user_dividir
+
+    while True:
+        soma_final_user_dividir = 0
+
+        if dividiu_par and "A" in cartas_user_dividir:
+
+            printar_cartas(cartas_user_dividir)
+            cartas_user_dividir.append(baralho_p_jogo.pop(0))
+            soma_final_user_dividir = logica_somar_cartas(cartas_user_dividir)
+            printar_cartas(cartas_user_dividir)
+            print()
+            break
+
+        soma_final_user_dividir = logica_somar_cartas(cartas_user_dividir)
+
+        printar_cartas(cartas_user_dividir)
+        print()
+
+        if soma_final_user_dividir >= 21:
+            print(f"Deu: {soma_final_user_dividir}")
+            break
+
+        while True:
+
+            input_usuario_dividir = input("\n(1)-Carta (2)-Ficar : ")
+
+            if input_usuario_dividir == "1" or input_usuario_dividir == "2":
+                break
+
+            else:
+                print("\nDigite um dos valores abaixo!")
+
+        print()
+
+        if input_usuario_dividir == "1":
+            cartas_user_dividir.append(baralho_p_jogo.pop(0))
+
+        elif input_usuario_dividir == "2":
+            printar_cartas(cartas_user_dividir)
+            print()
+            break
+
+
+# TODO: rever a posicao dessa funcao depois
+def dobrar_aposta():
+
+    if fichas_apostadas * 2 > quant_fichas_usuario:
+        print(f"Você não tem {fichas_apostadas} fichas sobrando para dobrar a aposta!\n")
+        return fichas_apostadas, False
+    else:
+        cartas_usuario.append(baralho_p_jogo.pop(0))
+        print()
+        return fichas_apostadas * 2, True
+
+
 print("Este eh um jogo de Blackjack!\n")
 print("Voce recebeu 50 fichas")
 
-input("Pressione ENTER para começar")
+# input("Pressione ENTER para começar")
 print("-" * 30)
 print()
 
@@ -156,21 +230,30 @@ while True:
 
         cartas_usuario.append(baralho_p_jogo.pop(0))
 
+    # TODO: remover esse while depois. isso ta aqui pra testar dividir_par
+    # while len(set(cartas_usuario)) != 1:
+    #     cartas_usuario.pop(0)
+    #     cartas_usuario.append(baralho_p_jogo.pop(0))
+
     fichas_apostadas = apostar_fichas()
 
     print(f"\n\nCartas do dealer: {cartas_dealer[0]} | #\n")
 
-    while True:
-        soma_final_usuario = 0
+    dealer_tem_Q_J_or_K = any(char in cartas_dealer for char in ["Q", "j", "K"])
 
-        # if cartas_usuario.count("A") == 1 and cartas_usuario[-1] == "A":
+    dealer_tem_21 = "A" in cartas_dealer and dealer_tem_Q_J_or_K
 
-        dealer_tem_Q_J_or_K = "K" in cartas_dealer or "Q" in cartas_dealer or "J" in cartas_dealer
+    while not dealer_tem_21:
 
-        if "A" in cartas_dealer and dealer_tem_Q_J_or_K:
+        if dividiu_par and len(cartas_usuario) == cartas_usuario.count("A"):
+            printar_cartas(cartas_usuario)
+            cartas_usuario.append(baralho_p_jogo.pop(0))
+            soma_final_usuario = logica_somar_cartas(cartas_usuario)
+            printar_cartas(cartas_usuario)
+            print()
             break
 
-        soma_final_usuario = logica_somar_cartas(cartas_usuario, soma_final_usuario)
+        soma_final_usuario = logica_somar_cartas(cartas_usuario)
 
         printar_cartas(cartas_usuario)
         print()
@@ -179,12 +262,22 @@ while True:
             print(f"Deu: {soma_final_usuario}")
             break
 
+        inputs_permitidos = ["1", "2", "3", "4"] if not dividiu_par else ["1", "2"]
+        msg_input_usuario = "\n(1)-Carta (2)-Ficar (3)-Dobrar : "
+
+        if run_uma_vez and len(set(cartas_usuario)) == 1:
+            msg_input_usuario = "\n(1)-Carta (2)-Ficar (3)-dobrar (4)-Dividir par : "
+
+        elif dividiu_par:
+            msg_input_usuario = "\n(1)-Carta (2)-Ficar : "
+
         while True:
 
-            # input_usuario = input("\nEscolha: (1)-Pedir carta (2)-Ficar (3)-Dobrar : ")
-            input_usuario = input("\nEscolha: (1)-Pedir carta (2)-Ficar : ")
+            input_usuario = input(msg_input_usuario)
 
-            if input_usuario == "1" or input_usuario == "2":
+            if input_usuario in inputs_permitidos:
+
+                run_uma_vez = False
                 break
 
             else:
@@ -200,41 +293,105 @@ while True:
             print()
             break
 
-    if soma_final_usuario <= 21:
+        elif input_usuario == "3":
+            fichas_apostadas, x = dobrar_aposta()
+            if x:
+                soma_final_usuario = logica_somar_cartas(cartas_usuario)
+                printar_cartas(cartas_usuario)
+                break
+
+        elif input_usuario == "4":
+
+            # TODO: tentar tirar esse if daqui
+            if fichas_apostadas * 2 > quant_fichas_usuario:
+                print(f"Voce nao tem {fichas_apostadas} fichas sobrando para dividir um par!\n")
+            else:
+                dividiu_par = True
+                cartas_user_dividir.append(cartas_usuario.pop(0))
+                print("Jogo 1:\n")
+
+    if len(cartas_user_dividir) == 1:
+        dividir_par()
+        fichas_apostadas_dividir = fichas_apostadas + 0
+
+    if soma_final_usuario <= 21 or dividiu_par and soma_final_user_dividir <= 21:
 
         print("\nDealer jogando:\n")
 
         while True:
 
-            soma_final_dealer = 0
-
-            soma_final_dealer = logica_somar_cartas(cartas_dealer, soma_final_dealer)
+            soma_final_dealer = logica_somar_cartas(cartas_dealer)
 
             printar_cartas(cartas_dealer)
             sleep(1.5)
 
             if soma_final_dealer >= 21:
-                print(f"\ndeu: {soma_final_dealer}\n")
+                print(f"\ndeu: {soma_final_dealer}")
                 break
 
-            if soma_final_dealer > 17 and soma_final_dealer >= soma_final_usuario:
+            maior_17_maior_user = 17 <= soma_final_dealer >= soma_final_usuario
+            maior_17_maior_user_dividir = 17 <= soma_final_dealer >= soma_final_user_dividir
+
+            if dividiu_par:
+
+                if soma_final_usuario > 21 and maior_17_maior_user_dividir:
+                    break
+
+                elif soma_final_user_dividir > 21 and maior_17_maior_user:
+                    break
+
+                elif maior_17_maior_user and maior_17_maior_user_dividir:
+                    break
+
+            elif maior_17_maior_user:
                 break
 
-            if soma_final_dealer <= 17:
+            if soma_final_dealer < 17:
                 # print("menor que 17")
                 cartas_dealer.append(baralho_p_jogo.pop(0))
+
+            elif dividiu_par:
+                if 21 >= soma_final_usuario > soma_final_dealer:
+                    cartas_dealer.append(baralho_p_jogo.pop(0))
+
+                elif 21 >= soma_final_user_dividir > soma_final_dealer:
+                    cartas_dealer.append(baralho_p_jogo.pop(0))
 
             elif soma_final_dealer < soma_final_usuario:
                 # print("menos q usuario")
                 cartas_dealer.append(baralho_p_jogo.pop(0))
 
-    resultado_final = definir_mensagem_resultado()
+    print("\n\nResultado(s):")
 
-    fichas_obtidas = calcular_fichas_obtidas(fichas_apostadas)
+    if len(cartas_user_dividir) >= 1:
 
-    quant_fichas_usuario += fichas_obtidas
+        print("\nJogo 1:")
+        resultado_final = definir_mensagem_resultado(soma_final_usuario)
 
-    print(f"{resultado_final}  | {'+' if fichas_obtidas > 0 else ''}{fichas_obtidas} ficha(s)")
+        fichas_obtidas = calcular_fichas_obtidas(fichas_apostadas)
+
+        quant_fichas_usuario += fichas_obtidas
+
+        print(f"{resultado_final}  | {'+' if fichas_obtidas > 0 else ''}{fichas_obtidas} ficha(s)")
+        print("¨" * 30)
+
+        print("\nJogo 2:")
+        resultado_final = definir_mensagem_resultado(soma_final_user_dividir)
+
+        fichas_obtidas = calcular_fichas_obtidas(fichas_apostadas)
+
+        quant_fichas_usuario += fichas_obtidas
+
+        print(f"{resultado_final}  | {'+' if fichas_obtidas > 0 else ''}{fichas_obtidas} ficha(s)")
+
+    else:
+        resultado_final = definir_mensagem_resultado(soma_final_usuario)
+
+        fichas_obtidas = calcular_fichas_obtidas(fichas_apostadas)
+
+        quant_fichas_usuario += fichas_obtidas
+
+        print(f"{resultado_final}  | {'+' if fichas_obtidas > 0 else ''}{fichas_obtidas} ficha(s)")
 
     if len(baralho_p_jogo) < 78:
         baralho_p_jogo = um_baralho * 6
@@ -244,11 +401,15 @@ while True:
     # resetar_variaveis()
 
     cartas_usuario = []
+    cartas_user_dividir = []
     cartas_dealer = []
     resultado_final = ""
     fichas_obtidas = 0
     soma_final_dealer = 0
+    soma_final_usuario = 0
     fichas_apostadas = 0
+    run_uma_vez = True
+    dividiu_par = False
 
     print("¨" * 30)
     print()
